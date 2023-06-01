c1acda96c7a1e26f82daf0ad78f9f07a22aa9ee1
CraciDenisStefan <crdenis13@gmail.com>
2023-06-01 15:12:17 +0200

    Snack 1

--- Snack1/js/script.js
+++ Snack1/js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+let vip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'] 
+
+let tavoloVip = vip.map((nomeVip, i) =>{
+    let posto ={
+        'nometavolo': 'VIP',
+        'nome' : nomeVip,
+        'posto': (i++)+1,
+    }
+    return posto
+ 
+})
+
+for(let dati in tavoloVip){
+    console.log(`Tavolo: ${tavoloVip[dati].nometavolo} Nome: ${tavoloVip[dati].nome} Posto: ${tavoloVip[dati].posto}`)
+   }
+ 
