f52d8bca703d0cd96b6b5e1de117335068e2cf90
CraciDenisStefan <crdenis13@gmail.com>
2023-06-05 15:12:58 +0200

    Snack3 completato

--- Snack3/js/script.js
+++ Snack3/js/script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const biciclette = [
+    { nome: 'Bici1', peso: 9 },
+    { nome: 'Bici2', peso: 4 },
+    { nome: 'Bici3', peso: 6 },
+    { nome: 'Bici4', peso: 8 },
+    { nome: 'Bici5', peso: 10 },
+]
+
+
+
+let pesoMinore = biciclette[0];
+
+biciclette.forEach((bici) => {
+    if (bici.peso < pesoMinore.peso) {
+      pesoMinore = bici;
+    }
+  });
+
+  let {nome, peso} = pesoMinore
+
+console.log(`La bici con il peso minore è ${nome} e pesa: ${peso}kg`)
+
+
+
+
